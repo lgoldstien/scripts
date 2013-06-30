c0cc5cf5c5d313536195cb9e239d7919424db3e6
lgoldstien <lgoldstien@gmail.com>
2013-06-30 17:18:56 +0200

    Add the set up script to get your system prepared

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+# Get your scripts up and running
+# @package scripts
+# @version 0.1
+# @author lgoldstien
+
+# Import our common set of functions
+import os
+import sys
+import common
+
